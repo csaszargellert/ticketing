785badeae4dde27a31849fe614dba337533f8bbf
Gellért Császár <csaszargellert@gmail.com>
2024-01-09 03:05:02 +0100

    change baseUrl to point to gellert-ticketing.online

--- client/src/api/buildClient.js
+++ client/src/api/buildClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const buildClient = function ({ req }) {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://ingress-nginx-srv',
+      baseURL: 'http://www.gellert-ticketing.online',
       headers: req.headers,
     });
   } else {
